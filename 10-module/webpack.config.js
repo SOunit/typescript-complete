6fcabe544e6a592feb64c3161a6628b2431fcbb9
SOunit <workbookwork7@gmail.com>
2022-05-12 12:22:39 -0700

    add build setting for webpack

--- 10-module/webpack.config.js
+++ 10-module/webpack.config.js
@@ -3,10 +3,16 @@
 const path = require('path');
 
 module.exports = {
-  entry: './dist/main.js',
+  entry: './src/foot-app/main.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'inline-source-map',
+  module: {
+    rules: [{ test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ }],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
 };
