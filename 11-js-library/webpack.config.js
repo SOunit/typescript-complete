145a5ae7c0d37e8ca9597cb24dce3e53e3ece769
SOunit <workbookwork7@gmail.com>
2022-05-12 12:23:39 -0700

    add new project

---
+++ 11-js-library/webpack.config.js
@@ -0,0 +1,18 @@
+// for multi os and different file system
+// some os do not use / for directory
+const path = require('path');
+
+module.exports = {
+  entry: './src/foot-app/main.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [{ test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ }],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
