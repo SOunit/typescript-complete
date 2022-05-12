89b480f77108583b7f74748565d01b01fe5d9261
SOunit <workbookwork7@gmail.com>
2022-05-12 12:06:13 -0700

    add sample

---
+++ 10-module/webpack.config.js
@@ -0,0 +1,11 @@
+// for multi os and different file system
+// some os do not use / for directory
+const path = require('path');
+
+module.exports = {
+  entry: './dist/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
