0c326209acb2b36a82c01eac385f5ea661c15b7d
SOunit <workbookwork7@gmail.com>
2022-05-12 16:10:04 -0700

    add d.ts file

--- 11-js-library/src/library.ts
+++ 11-js-library/src/library.ts
@@ -6,4 +6,7 @@ axios.get('');
 // 2. no d.ts file offered
 // not typed
 // - search type : npm i --save-dev @types/lodash
+
+// 3. manual type definition
 import _ from 'lodash';
+_.shuffle([1, 2, 3]);
---
+++ 11-js-library/src/lodash.d.ts
@@ -0,0 +1,3 @@
+declare module 'lodash' {
+  export function shuffle<T>(err: T[]): T[];
+}
