b71b8eb5cda30390135528038f3d9a56963dd3eb
SOunit <workbookwork7@gmail.com>
2022-05-12 18:52:47 -0700

    add sample

--- 11-js-library/src/library.ts
+++ 11-js-library/src/library.ts
@@ -1,6 +1,8 @@
 // 1. d.ts file offered
 // already typed
 import axios from 'axios';
+import _ from 'lodash';
+
 axios.get('');
 
 // 2. no d.ts file offered
@@ -8,5 +10,15 @@ axios.get('');
 // - search type : npm i --save-dev @types/lodash
 
 // 3. manual type definition
-import _ from 'lodash';
-_.shuffle([1, 2, 3]);
+// import _ from 'lodash';
+// _.shuffle([1, 2, 3]);
+
+// 4. cdn
+console.log(_.shuffle([1, 2, 3]));
+
+namespace myApp {
+  const hello = 'hello in name space';
+  export const name = 'name in name space';
+}
+
+const hello = 'hello in global' + myApp.name;
--- 11-js-library/src/lodash.d.ts
+++ 11-js-library/src/lodash.d.ts
@@ -1,3 +1,19 @@
-declare module 'lodash' {
-  export function shuffle<T>(err: T[]): T[];
-}
+// declare module 'lodash' {
+//   export function shuffle<T>(err: T[]): T[];
+// }
+
+// // pattern 1
+// // declare const _: {
+// //   shuffle<T>(arr: T[]): T[];
+// // };
+
+// // pattern 2
+// // interface Lodash {
+// //   shuffle<T>(arr: T[]): T[];
+// // }
+// // declare const _: Lodash;
+
+// // pattern 3
+// declare namespace _ {
+//   function shuffle<T>(arr: T[]): T[];
+// }
