64e4433d7ea7b9862dc095e486a1d77207db47fc
SOunit <workbookwork7@gmail.com>
2022-06-01 14:15:22 -0700

    add non-null sample

---
+++ 06-advanced/src/76-non-null.ts
@@ -0,0 +1,8 @@
+namespace tmp {
+  const input = document.getElementById('input')!;
+  const input2 = document.getElementById('input') as HTMLInputElement;
+}
+
+// 1. not-null assertion
+// 2. same with non-null assertion
+// 3. have to be careful to use in both cases
