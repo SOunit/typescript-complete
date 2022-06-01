83e06d5fa878e3ca01b5e6572fd26a046a5c5b2a
SOunit <workbookwork7@gmail.com>
2022-06-01 10:19:12 -0700

    add sample

--- 04-class/src/class.ts
+++ 04-class/src/class.ts
@@ -1,11 +1,23 @@
 class Person {
   name: string;
+  private age: number;
 
-  constructor(initName: string) {
+  constructor(initName: string, initAge: number) {
     this.name = initName;
+    this.age = initAge;
+  }
+
+  incrementAge() {
+    this.age++;
+  }
+
+  // type for this syntax
+  greeting(this: Person) {
+    console.log(`hello my name is ${this.name}, ${this.age} years old!`);
   }
 }
 
-const jack = new Person('Jack');
-
-console.log(jack);
+const jack = new Person('Jack', 38);
+jack.greeting();
+jack.incrementAge();
+jack.greeting();
