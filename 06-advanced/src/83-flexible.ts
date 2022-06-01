aff53061450b5275e1fa1e4a104562ce5eb3df72
SOunit <workbookwork7@gmail.com>
2022-06-01 15:25:23 -0700

    add sample

---
+++ 06-advanced/src/83-flexible.ts
@@ -0,0 +1,12 @@
+namespace tmp {
+  const str = '1';
+  const num = '1';
+  const sum = str + num;
+  console.log(sum);
+
+  const arr = ['1', '2', '3'];
+  const sum2 = arr + str;
+  console.log(sum2);
+}
+
+// TS respect flexibility of JS
