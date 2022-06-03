4d6ed0b13bcd1e3b3a81bdd5604e9264d72a19a8
SOunit <workbookwork7@gmail.com>
2022-06-03 08:21:32 -0700

    add sample

--- 13-node/src/app.ts
+++ 13-node/src/app.ts
@@ -1,8 +1,33 @@
-import express from 'express';
+import express, { json, NextFunction, Request, Response } from 'express';
 
 const PORT = 5000;
 
+interface MessageRequest extends Request {
+  body: {
+    message: string;
+  };
+}
+
 const app = express();
+app.use(json());
+
+app.use((req, res, next) => {
+  console.log(`request ${req.method} ${req.baseUrl} ${req.url} `);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('<h1>hello</h1>');
+});
+
+app.post('/', (req: MessageRequest, res) => {
+  console.log(req.body);
+  res.send(`<h1>I got ${req.body.message}</h1>`);
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`listen on ${PORT}`);
